718cf90dc4b645814d3ef1da1a37aac223e2d61e
mishra-khushboo <khush27093@gmail.com>
2025-10-10 23:29:23 +0530

    showed all listing title

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Veranda";
 main()
@@ -14,13 +15,16 @@ main()
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
   res.send("i am root");
 });
 
 app.get("/listing", async (req, res) => {
   const allListings = await Listing.find({});
-  res.render("index.ejs", { allListings });
+  res.render("listings/index.ejs", { allListings });
 });
 app.listen(8080, () => {
   console.log("server is running");
