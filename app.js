a3440eac3e5d6df6a4a9e80b03289e51329e66f3
mishra-khushboo <khush27093@gmail.com>
2025-10-12 14:56:06 +0530

    new listing can be created

--- app.js
+++ app.js
@@ -38,6 +38,14 @@ app.get("/listings/:id", async (req, res) => {
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
 });
+//create route
+app.post("/listings", async (req, res) => {
+  //let { title, description, image, price, country, location } = req.body;
+  //let listing = req.body.listing;
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listing");
+});
 
 app.listen(8080, () => {
   console.log("server is running");
